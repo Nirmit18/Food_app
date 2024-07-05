6b4c4ad0282af180248db492aa36e2c6d93753ca
Nirmit18 <nimitkaundalrock2004@gmail.com>
2024-07-05 15:49:11 +0530

    search added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import ChildB from './components/ChildB';
 import ChildC from './components/ChildC';
 import ChildD from './components/ChildD';
+import Search from './components/Search';
 
 
 
@@ -21,6 +22,7 @@ function App() {
         <Route path='/b' element={<ChildB />} />
         <Route path='/c' element={<ChildC />} />
         <Route path='/d' element={<ChildD />} />
+        <Route path='/search' element={<Search />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Search.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Search = (props) => {
+    const [val, setVal] = useState("");
+    const [data, setData] = useState([]);
+
+    const handleChange = (e) => {
+        setVal(e.target.value);
+    };
+
+    const searchVal = async () => {
+        try {
+            const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${val}`);
+            setData(response.data.meals);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    useEffect(() => {
+        console.log(val);
+    }, [val]);
+
+    return (
+        <div className="bg-neutral-200 min-h-screen">
+            <div className="relative max-w-lg mx-auto  flex bg-neutral-300">
+                <input
+                    className="w-full py-2 px-4 border border-gray-300 rounded-md shadow-sm focus:outline-none"
+                    type="search"
+                    placeholder="Search"
+                    value={val}
+                    onChange={handleChange}
+                />
+                <button className="bg-red-600 rounded-lg p-8 pt-0 pb-0 text-white font-semibold "
+                    onClick={searchVal}
+                >
+                    Search
+                </button>
+            </div>
+            <div>{val}</div>
+
+            {data.map((meal) => (
+                    <div key={meal.idMeal}>{meal.strMeal}</div>
+                ))}
+        </div>
+    );
+};
+
+export default Search;
